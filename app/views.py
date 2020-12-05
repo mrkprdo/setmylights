98424991c0f5d116810c4da5d88127b7c17939f1
Ubuntu <ubuntu@ip-172-31-76-179.ec2.internal>
2020-12-05 02:57:07 +0000

    Commit to working version

--- app/views.py
+++ app/views.py
@@ -1,10 +1,40 @@
 from . import app
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, request, redirect, url_for, jsonify
+import string
 
+def read():
+    with open('color_state','r') as ff:
+        return ff.read()
+
+def save(color):
+    #data sanitation
+    check_hex = [n in string.hexdigits for n in color[1:6]]
+    if len(color) == 7 and color[0] == '#' and all(check_hex):
+        with open('color_state','w+') as ff:
+            ff.write(color)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    COLOR_STATE = read()
+    return render_template('index.html',hexcolor=COLOR_STATE)
+
+@app.route('/set', methods=['POST'])
+def set_value():
+    data = request.form.to_dict(flat=False)
+    save(data['color'][0])
+    COLOR_STATE = read()
+    return render_template('index.html',hexcolor=COLOR_STATE)
+
+@app.route('/get', methods=['GET'])
+def get_value():
+    COLOR_STATE = read()
+    hex_color = COLOR_STATE.lstrip('#')
+    rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    data = {
+        'hex_color':COLOR_STATE,
+        'rgb_color':rgb_color
+    }
+    return jsonify(data)
 
 # @app.route('/farbtastic.css')
 # def farbcss():
